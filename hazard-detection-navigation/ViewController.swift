8f75cb6283935bb7095f751c105e38569a7659c8
SeongHuiJung <tjdgml150237@naver.com>
2024-03-14 10:13:55 +0900

    Initial Commit

---
+++ hazard-detection-navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hazard-detection-navigation
+//
+//  Created by 정성희 on 2024/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
